4fb29760b51b6e05a7e92085c3caf86f5af34209
M Farkas-Dyck <strake888@gmail.com>
2017-10-14 20:58:22 -0800

    boxed+heap

--- src/lib.rs
+++ src/lib.rs
@@ -4,9 +4,13 @@
             issue = "27783")]
 
 #![feature(core_intrinsics)]
+#![feature(rustc_attrs)]
 #![feature(staged_api)]
+#![feature(test)]
 #![feature(unique)]
 
 #![no_std]
 
 pub mod allocator;
+pub mod boxed;
+pub mod heap;
